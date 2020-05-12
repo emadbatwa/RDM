ab579db6e328288d5ce76cd9c01c3348fd47d355
dina244 <50928946+dina244@users.noreply.github.com>
2020-05-12 09:22:13 +0300

    update popup for Admin

--- resources/views/admin/list.blade.php
+++ resources/views/admin/list.blade.php
@@ -25,6 +25,14 @@ function getid (ele) {
     console.log(ticket['ticket'].description);
     console.log(ticket);
     $('#description').text(ticket['ticket'].description);
+    $('#assigned_company').text(ticket['ticket'].assigned_company); // @@@@@@@
+    $('#classification_ar').text(ticket['ticket'].classification_ar);
+    $('#degree_ar').text(ticket['ticket'].degree_ar);
+    $('#status_ar').text(ticket['ticket'].status_ar);
+    $('#created_at').text(ticket['ticket'].created_at);
+    $('#updated_at').text(ticket['ticket'].updated_at);
+    $('#id').text(ticket['ticket'].id);
+   
 }
 
 </script>
@@ -111,6 +119,7 @@ function getid (ele) {
                                 <th>الحالة</th>
                                 <th>الدرجة</th>
                                 <th>التصنيف</th>
+                                <th>الشركة</th>
                                 <th>تاريخ الإنشاء</th>
                             </tr>
                             </thead>
@@ -124,6 +133,7 @@ function getid (ele) {
                                     <td>{{$ticket['ticket']->status_ar}}</td>
                                     <td>{{$ticket['ticket']->degree_ar}}</td>
                                     <td>{{$ticket['ticket']->classification_ar}}</td>
+                                    <td>{{$ticket['ticket']->assigned_company}}</td>
                                     <td>{{$ticket['ticket']->created_at}}</td>
                                 </tr>
                             @endforeach
@@ -135,6 +145,7 @@ function getid (ele) {
                                 <th>الحالة</th>
                                 <th>الدرجة</th>
                                 <th>التصنيف</th>
+                                <th>الشركة</th>
                                 <th>تاريخ الإنشاء</th>
                             </tr>
                             </tfoot>
@@ -145,46 +156,51 @@ function getid (ele) {
     <div class="modal-content">
       <div class="modal-header">
         <h4 class="modal-title"> تذكرة رقم:  </h4>
+        <h4 id="id">  </h4>
         <h7 class="modal-title">الحالة:  </h7>
+        <h7 id="status_ar"> </h7> 
+
         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
       </div>
 
     <p>
     <table class="table">
     <tr>
-    <th>تاريخ الإصدار:</th>
-    <td>1\1</td>
+    <th>تاريخ الإنشاء:</th>
+    <td id="created_at"></td>
     <th>تاريخ التعديل:</th>
-    <td>1\2</td>
+    <td id="updated_at"></td>
     <th>صور البلاغ</th>
   </tr>
   <tr>
     <th>التصنيف:</th>
+    <td id="classification_ar"></td>
     <td>
 
     <div class="btn-group">
         <select  type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <option value="makkah" selected >1</a>
-        <option value="madina">2</a>
-        <option value="jeddah">3</a>
+        <option  selected >1</a>
+        <option >2</a>
+        <option >3</a>
         </select>
     </div>
 
 </td>
         <th>حجم الضرر:</th>
-        <td>عالي</td>
+        <td id="degree_ar"></td>
       <th>الوصف</th>
       <td id="description"></td>
         <td rowspan="2"> الصور </td>
   </tr>
   <tr>
-    <th>الموظف المسؤول:</th>
+    <th>الشركة:</th>
+    <td id="assigned_company"></td>
     <td>
     <div class="btn-group">
   <select  type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    <option value="makkah" selected >1</a>
-    <option value="madina">2</a>
-    <option value="jeddah">3</a>
+    <option id="assigned_company" selected >1</a>  <!-- @@@@@@@ -->
+    <option >2</a>
+    <option >3</a>
 </select>
 </div>
     </td>
