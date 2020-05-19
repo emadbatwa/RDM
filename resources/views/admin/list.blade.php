7376bb8d9f04680bc7da206c19df7b9911de06fd
Mohammed Daggas <48481584+hmooddes@users.noreply.github.com>
2020-05-19 08:17:53 +0300

    test 1Million

--- resources/views/admin/list.blade.php
+++ resources/views/admin/list.blade.php
@@ -54,7 +54,22 @@
         </div>
 
 
-        @include('layouts.Side-bar-toggle')
+{{--        @include('layouts.Side-bar-toggle')--}}
+
+        <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="navbar-toggler-icon icon-bar"></span>
+                    <span class="navbar-toggler-icon icon-bar"></span>
+                    <span class="navbar-toggler-icon icon-bar"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end ">
+                </div>
+            </div>
+        </nav>
 
         <script>
             $(document).ready(function () {
